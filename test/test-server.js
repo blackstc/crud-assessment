0ae60113632ef3e09ff85167155c0c64c1969989
Blackstock <charlieblackstock@gmail.com>
2015-10-15 15:57:06 -0600

    updating test suite

--- test/test-server.js
+++ test/test-server.js
@@ -3,11 +3,10 @@ process.env.NODE_ENV = 'test';
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var mongoose = require("mongoose");
-
 var server = require('../server/app');
 var Pokemon = require("../server/database");
-
 var should = chai.should();
+
 chai.use(chaiHttp);
 
 
